1f0dc67796ddfc8a1cd5b9607f397e8af1fdea9f
ArthurX10 <153871082+ArthurX10@users.noreply.github.com>
2024-11-17 13:52:30 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,35 +1,48 @@
 #include<stdio.h>
-#include <stdlib.h>
+#include<stdlib.h>
 
-int main() {
-    int tam, *vet, i;
 
-    scanf("%d", &tam);
+int main(){
+    int **mat, linha, coluna, i, j;
 
-     if (tam ==0) {
-        printf("[vazio]");
-        return;
+
+    scanf("%d %d", &linha, &coluna);
+
+
+    if(linha == 0 || coluna==0){
+        printf("[matriz vazia]");return 0;
     }
 
-    vet = malloc(tam * sizeof(int));
-
-    if (vet) {
-        for (i = 0; i < tam; i++) {
-            scanf("%d", &vet[i]);
+    mat = (int**)malloc(linha*sizeof(int*));
+        for(int i = 0; i< linha; i++){
+            mat[i] = (int*)malloc(coluna *sizeof(int));
         }
 
-        printf("[");
-        for (i = 0; i < tam; i++) {
-            printf("%d", vet[i]);
-            if (i < tam - 1) {
-                printf(", ");
+
+       for(i = 0; i < linha; i++){
+           for(j = 0; j< coluna; j++){
+                scanf("%d", &mat[i][j]);
+        }
+        }
+         printf("'");
+        for(i = 0; i < linha; i++){
+           for(j = 0; j < coluna; j++){
+                printf("%d", mat[i][j]);
+                if (j < coluna - 1) {
+                printf(" ");
             }
         }
-        printf("]\n");
-
-        free(vet);
+        if (i < linha - 1) {
+            printf("\n");
+        } else {
+        printf("'");}
     }
 
-    return 0;
+        for (i = 0; i < linha; i++) {
+        free(mat[i]);
+    }
+    free(mat);
+return 0;
 }
+
 
