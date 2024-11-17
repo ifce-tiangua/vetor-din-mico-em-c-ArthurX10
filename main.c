11fe39ac1d5642b008f7b9e35689fadb72cc84c8
ArthurX10 <153871082+ArthurX10@users.noreply.github.com>
2024-11-17 12:37:09 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,25 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 int main(){
+    int tam, *vet, i;
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+    scanf("%d", &tam);
+
+    vet = malloc(tam * sizeof(int));
+
+    if (vet) {
+        for (i = 0; i < tam; i++) {
+            scanf("%d", &vet[i]);
+        }
+
+        for (i = 0; i < tam; i++) {
+            printf("%d ", vet[i]);
+        }
+        printf("\n");
+
+        free(vet);
+    }
 
     return 0;
 }
